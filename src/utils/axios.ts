114d76f2f8728163b51b61c370a9bb5adb32490e
licong <284264018@qq.com>
2021-08-31 15:40:48 +0800

    init

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@
  * @Author: 李聪
  * @Date: 2021-08-31 11:55:02
  * @LastEditors: 李聪
- * @LastEditTime: 2021-08-31 13:57:11
+ * @LastEditTime: 2021-08-31 15:40:40
  * @Description:axios配置
  */
 
@@ -48,7 +48,7 @@ axios.interceptors.response.use(
     } else {
       ElMessage.error(`${error}`);
     }
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
 );
 
