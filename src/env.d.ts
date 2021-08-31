64e0a41834ae0c8fff33032416782b438b740763
licong <284264018@qq.com>
2021-08-31 16:51:06 +0800

    init: vite+vue3+ts+elmentUI-plus+prettier+eslint+editor

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,15 @@
+/*
+ * @Author: 李聪
+ * @Date: 2021-08-31 10:57:39
+ * @LastEditors: 李聪
+ * @LastEditTime: 2021-08-31 16:47:16
+ * @Description:
+ */
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import { DefineComponent } from 'vue'
+  import { DefineComponent } from 'vue';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
--- src/views/Vuex.vue
+++ src/views/Vuex.vue
@@ -2,7 +2,7 @@
  * @Author: 李聪
  * @Date: 2021-08-31 11:28:07
  * @LastEditors: 李聪
- * @LastEditTime: 2021-08-31 11:28:08
+ * @LastEditTime: 2021-08-31 16:33:59
  * @Description:页面
 -->
 <template>
@@ -11,4 +11,8 @@
 
 <script setup lang="ts"></script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.vuex {
+  background: rgb(194, 154, 154);
+}
+</style>
