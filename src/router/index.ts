ba69adfa498db4e5354ecaedb641789111efa82d
licong <284264018@qq.com>
2021-09-01 11:42:12 +0800

    init: vite+vue3+ts+elmentUI-plus+prettier+eslint+editor

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,12 @@
  * @Author: 李聪
  * @Date: 2021-08-31 11:24:45
  * @LastEditors: 李聪
- * @LastEditTime: 2021-09-01 11:39:56
+ * @LastEditTime: 2021-09-01 11:42:06
  * @Description: 路由配置
  */
 
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Home from '@/view/Home.vue';
-import About from '@/view/About.vue';
 
 const routes: RouteRecordRaw[] = [
   {
